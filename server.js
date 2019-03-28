dd971bd75af57fd8f05102a8e17e6c15df09ae5e
Steve Archuleta <stevearchuleta@me.com>
2019-03-28 13:17:46 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("app/public"));
+
+
+
+
+
+
+
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
